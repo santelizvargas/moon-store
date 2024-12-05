e07402f8094ede389da9c49ae9a611a273544b48
santelizvargas <ushija6@gmail.com>
2024-12-04 21:39:24 -0600

    feat: Add Screen types

--- moon-store-mac-os/moon-store-mac-os/Views/Main/Screen.swift
+++ moon-store-mac-os/moon-store-mac-os/Views/Main/Screen.swift
@@ -5,10 +5,38 @@
 //  Created by Steven Santeliz on 17/11/24.
 //
 
-enum Screen: String, Identifiable, CaseIterable {
-    case first, second, third
-
+enum ScreenSection: String, Identifiable, CaseIterable {
+    case overview = "Vista General"
+    case inventory = "Inventario"
+    case billing = "Facturación"
+    
     var id: String { rawValue }
     
-    var title: String { rawValue.capitalized }
+    var screens: [Screen] {
+        switch self {
+            case .overview: [.charts, .users]
+            case .inventory: [.products]
+            case .billing: [.newSale, .salesReport]
+        }
+    }
+}
+
+enum Screen: String, Identifiable {
+    case charts = "Gráficas"
+    case users = "Usuarios"
+    case products = "Lista de Productos"
+    case newSale = "Nueva Venta"
+    case salesReport = "Reporte de Ventas"
+    
+    var id: String { rawValue }
+    
+    var iconName: String {
+        switch self {
+            case .charts: "square.grid.2x2"
+            case .users: "person"
+            case .products: "list.bullet.clipboard"
+            case .newSale: "plus.circle"
+            case .salesReport: "chart.line.uptrend.xyaxis"
+        }
+    }
 }
