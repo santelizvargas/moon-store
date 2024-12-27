7a65772b1ecfecd629156d7f9df4c1f746a1bd5e
Jose Luna <lunajose6969@gmail.com>
2024-12-27 16:51:39 -0600

    chore: Feedback

--- moon-store-mac-os/moon-store-mac-os/Helpers/AlertManager/AlertManager.swift
+++ moon-store-mac-os/moon-store-mac-os/Helpers/AlertManager/AlertManager.swift
@@ -8,24 +8,16 @@
 import AppKit
 
 final class AlertPresenter {
-    private static var isPresented: Bool = false
     
     private init() { }
     
     static func showAlert(type: AlertType, alertMessage: String) {
-        isPresented = true
-        alertConstruction(type: type, message: alertMessage)
-    }
-    
-    private static func alertConstruction(type: AlertType, message: String) {
         let alert = NSAlert()
         alert.messageText = type.title
-        alert.informativeText = message
+        alert.informativeText = alertMessage
         
         alert.icon = NSImage(named: type.icon)
         alert.addButton(withTitle: "OK")
         alert.runModal()
-        
-        isPresented = false
     }
 }
